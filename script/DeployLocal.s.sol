4306d03520d5cdbbae2c9826016aef2ae2ace5c5
Samuel Taylor <sam@darkfunction.com>
2022-08-26 20:48:40 +0100

    Start writing tests and deployment scripts in Foundry

---
+++ script/DeployLocal.s.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import "../src/Raffle.sol";
+import "chainlink/mocks/VRFCoordinatorV2Mock.sol";
+
+contract DeployLocal is Script {
+    /* Constants */
+    uint256 public immutable ENTRANCE_FEE = 0.1 ether;
+    uint96 public immutable BASE_FEE = 0.25 ether;
+    uint96 public immutable GAS_PRICE_LINK = 1000000000;
+    bytes32 public immutable GOERLI_GAS_LANE =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint64 public immutable GOERLI_SUBSCRIPTION_ID = 580;
+    uint32 public immutable CALLBACK_GAS_LIMIT = 500000;
+    uint256 public immutable INTERVAL = 30;
+
+    /* State variables */
+    VRFCoordinatorV2Interface public coordinator;
+    Raffle public raffle;
+
+    function run() external {
+        vm.startBroadcast();
+
+        console.log("Deploying mock VRFCoordinatorV2");
+        coordinator = VRFCoordinatorV2Interface(
+            new VRFCoordinatorV2Mock(BASE_FEE, GAS_PRICE_LINK)
+        );
+        console.log("Coordinator deployed:", address(coordinator));
+
+        console.log("Creating mock VRF subscription");
+        uint64 subId = coordinator.createSubscription();
+        console.log("Mock subscription created: ", subId);
+
+        console.log("Deploying Raffle");
+        raffle = new Raffle(
+            address(coordinator),
+            0.1 ether,
+            GOERLI_GAS_LANE,
+            subId,
+            CALLBACK_GAS_LIMIT,
+            INTERVAL
+        );
+        console.log("Deployed Raffle contract");
+
+        vm.stopBroadcast();
+    }
+}
--- test/Raffle.t.sol
+++ test/Raffle.t.sol
@@ -2,11 +2,27 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Raffle.sol";
+import "chainlink/mocks/VRFCoordinatorV2Mock.sol";
+import "../script/DeployLocal.s.sol";
+import {console} from "forge-std/console.sol";
 
 contract RaffleTest is Test {
-    function setUp() public {}
+    /* State variables */
+    DeployLocal private deployment;
+    VRFCoordinatorV2Interface private vrfCoordinatorMock;
+    Raffle private raffle;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        deployment = new DeployLocal();
+        deployment.run();
+        raffle = deployment.raffle();
+        vrfCoordinatorMock = deployment.coordinator();
+    }
+
+    function test_whenInitialised_thenStateIsCorrect() public {
+        console.log(""); //raffle.getRaffleState());
+        assertEq(raffle.getRaffleState(), uint256(0));
+        assertEq(raffle.getEntranceFee(), deployment.ENTRANCE_FEE());
     }
 }
